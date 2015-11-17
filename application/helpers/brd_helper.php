73fa9476fb5f29d7c1e595cc78e62b76b6551143
aditya <adityaf.putra@gmail.com>
2015-11-17 18:31:13 +0700

    add breadcrumb helper

---
+++ application/helpers/brd_helper.php
@@ -0,0 +1,32 @@
+<?php
+
+// snippet from http://thephpx.com/2010/05/codeigniter-helper-create-breadcrumb-from-url/
+if(!function_exists('create_breadcrumb')){
+function create_breadcrumb(){
+  $ci = &get_instance();
+  $i=1;
+  $uri = $ci->uri->segment($i);
+  $link = '<ol class="breadcrumb">';
+
+  while($uri != ''){
+    $prep_link = '';
+  for($j=1; $j<=$i;$j++){
+    $prep_link .= $ci->uri->segment($j).'/';
+  }
+
+  if($ci->uri->segment($i+1) == ''){
+    $link.='<li>»<a href="'.site_url($prep_link).'"><b>';
+    $link.=$ci->uri->segment($i).'</b></a></li> ';
+  }else{
+    $link.='<li>» <a href="'.site_url($prep_link).'">';
+    $link.=$ci->uri->segment($i).'</a></li> ';
+  }
+
+  $i++;
+  $uri = $ci->uri->segment($i);
+  }
+    $link .= '</ol>';
+    return $link;
+  }
+}
+?>
