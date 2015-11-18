84ab2942ef8f8f08ff471dc67c9556f3e4532075
aditya <adityaf.putra@gmail.com>
2015-11-18 19:54:11 +0700

    add function to retrieve user's avatar url

--- application/helpers/adityaputra_helper.php
+++ application/helpers/adityaputra_helper.php
@@ -4,4 +4,20 @@ function pour($arr){
   print_r($arr);
   echo "</pre>";
 }
+
+function get_user_avatar_url($email){
+  $CI = get_instance();
+  $CI->load->model('admin/M_user');
+  $user = $CI->M_user->get(array('email' => $email));
+  $user = $user[0];
+  // pour($CI->session);
+  // pour($user); exit;
+  if(empty($user['avatar_loc'])){
+    return 'http://placehold.it/400/4444AA?text=BD';
+  }
+  else{
+    return(base_url().AVATAR_DIR.$user['avatar_loc']);
+  }
+  // exit;
+}
 ?>
